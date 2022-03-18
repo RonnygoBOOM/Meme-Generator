8764d7107b472c23ff92b9b40843e7b96d7c4810
Scheibelr <scheibelr@gmail.com>
2022-03-18 11:32:15 -0400

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import Header from './components/HeaderComponent';
+import Meme from './components/MemeComponent';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Meme />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/HeaderComponent.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function Header() {
+    return (
+        <>
+        <header className="header">
+            <img className="troll-face" src="images/troll-face.png"></img>
+            <h1 className="header--title">MEME Generator</h1>
+            <h4 className="header--project">React Course- Project 3</h4>
+        </header>
+        </>
+    )
+}
+
+export default Header;
