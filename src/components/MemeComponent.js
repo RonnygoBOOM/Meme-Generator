bc7fd9ba340a43e1ecfb65b84cee15662fa16138
Scheibelr <scheibelr@gmail.com>
2022-03-21 17:14:14 -0400

    Rendering Form Text

--- src/components/MemeComponent.js
+++ src/components/MemeComponent.js
@@ -25,13 +25,28 @@ export default function Meme() {
             })
     }
 
+    function handleTopText(event) {
+        setMeme(prev => {
+            return {...prev,
+            topText: event.target.value}
+        })
+    }
+
+    function handleBottomText(event) {
+        setMeme(prev => {
+            return {
+            ...prev,
+            bottomText: event.target.value}
+        })
+    }
+
     return (
         <main>
             <div className="form">
-                <input className="form--input" type="text" placeholder="Top text">
+                <input className="form--input" type="text" onChange={handleTopText} placeholder="Top text">
                 
                 </input>
-                <input className="form--input" type="text" placeholder="Bottom text">
+                <input className="form--input" type="text" onChange={handleBottomText} placeholder="Bottom text">
                 
                 </input>
                 <button onClick={getMemeImage} className="form--button">
@@ -39,9 +54,9 @@ export default function Meme() {
                 </button>
             </div>
             <div className="result">
-                <p>{meme.topText}</p>
+                <p classname="top-text">{meme.topText}</p>
                 <img className="meme-image" src={meme.randomImage}></img>
-                <p>{meme.bottomText}</p>
+                <p className="bottom-text">{meme.bottomText}</p>
             </div>
         </main>
     )
